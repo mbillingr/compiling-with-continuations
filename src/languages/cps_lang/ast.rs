9ed5fa85f4dceb8a2011b66c4de9d5d446e1aa92
Martin Billinger-Finke <mbillingr@users.noreply.github.com>
2023-08-23 13:45:24 +0200

    move the cps ast where it belongs

---
+++ src/languages/cps_lang/ast.rs
@@ -0,0 +1,24 @@
+use crate::core::reference::Ref;
+
+pub enum Value<V: 'static> {
+    Var(V),
+    Label(V),
+    Int(i64),
+    Real(f64),
+    String(Ref<String>),
+    Halt, // this represents a continuation that stops the program when called
+}
+
+type List<T> = Ref<[T]>;
+
+pub enum Expr<V: 'static> {
+    Record(List<Value<V>>, V, Ref<Expr<V>>),
+    Select(isize, Value<V>, V, Ref<Expr<V>>),
+    Offset(isize, Value<V>, V, Ref<Expr<V>>),
+    App(Value<V>, List<Value<V>>),
+    Fix(List<(V, List<V>, Expr<V>)>, Ref<Expr<V>>),
+    Switch(Value<V>, List<Expr<V>>),
+    PrimOp(PrimOp, List<Value<V>>, List<V>, List<Expr<V>>),
+}
+
+pub enum PrimOp {}
--- src/languages/mod.rs
+++ src/languages/mod.rs
@@ -1,2 +1,2 @@
-pub mod mini_lambda;
 pub mod cps_lang;
+pub mod mini_lambda;
