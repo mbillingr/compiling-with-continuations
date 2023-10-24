bd442f820916bfbad3c6515ebf875f55176068a6
Martin Billinger <mbillingr@users.noreply.github.com>
2023-10-24 15:40:09 +0200

    uncurry with args in record

--- src/transformations/cps_uncurrying.rs
+++ src/transformations/cps_uncurrying.rs
@@ -1,5 +1,5 @@
 use crate::core::reference::Ref;
-use crate::languages::cps_lang::ast::{Expr, Value};
+use crate::languages::cps_lang::ast::{AccessPath, Expr, Value};
 use crate::list;
 use crate::transformations::GensymContext;
 
@@ -43,6 +43,7 @@ impl Context {
                 for ((f, params, _), fbody) in defs.iter().zip(&reduced_bodies) {
                     match &**fbody {
                         // uncurrying ... this is the transformation on top of page 77
+                        //            ... with the extension that it creates a record for the arguments
                         Expr::Fix(
                             Ref([(g, Ref([b, k]), gbody)]),
                             Ref(Expr::App(Value::Var(c), Ref([Value::Var(gg) | Value::Label(gg)]))),
@@ -56,18 +57,33 @@ impl Context {
                             let g_ = self.gs.gensym(g);
                             let b_ = self.gs.gensym(b);
                             let k_ = self.gs.gensym(k);
+                            let args_ = self.gs.gensym("args");
+                            let pars_ = self.gs.gensym("args");
 
                             let mut f_args: Vec<Value<_>> =
                                 fparams.iter().map(|p| Value::Var(*p)).collect();
                             f_args.push(Value::Var(g_));
                             f_args.push(Value::Var(b_));
-                            f_args.push(Value::Var(k_));
 
                             let fbody_out = Expr::Fix(
                                 list![(
                                     g_,
                                     list![b_, k_],
-                                    Expr::App(Value::Var(f_), Ref::array(f_args)).into()
+                                    Expr::Record(
+                                        Ref::array(
+                                            f_args
+                                                .into_iter()
+                                                .map(|f| (f, AccessPath::Ref(0)))
+                                                .collect()
+                                        ),
+                                        args_,
+                                        Expr::App(
+                                            Value::Var(f_),
+                                            Ref::array(vec![Value::Var(args_), Value::Var(k_)])
+                                        )
+                                        .into()
+                                    )
+                                    .into()
                                 )],
                                 Expr::App(Value::Var(c_), list![Value::Var(g_)]).into(),
                             );
@@ -77,9 +93,19 @@ impl Context {
                             let mut f_params: Vec<_> = params.iter().copied().collect();
                             f_params.push(*g);
                             f_params.push(*b);
-                            f_params.push(*k);
 
-                            defs_out.push((f_, Ref::array(f_params), self.uncurry(gbody).into()));
+                            let mut body_out = Ref::new(self.uncurry(gbody));
+
+                            for (i, p) in f_params.into_iter().enumerate().rev() {
+                                body_out = Ref::new(Expr::Select(
+                                    i as isize,
+                                    Value::Var(pars_),
+                                    p,
+                                    body_out,
+                                ));
+                            }
+
+                            defs_out.push((f_, Ref::array(vec![pars_, *k]), body_out));
                         }
 
                         _ => defs_out.push((f.clone(), *params, *fbody)),
@@ -117,7 +143,10 @@ impl Context {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{cps_expr, cps_expr_list, cps_ident_list, cps_value, cps_value_list};
+    use crate::{
+        cps_expr, cps_expr_list, cps_field, cps_field_list, cps_ident_list, cps_value,
+        cps_value_list,
+    };
 
     fn uncurry(exp: Expr<Ref<str>>) -> Expr<Ref<str>> {
         let ctx = Box::leak(Box::new(Context::new("__".to_string())));
@@ -127,7 +156,18 @@ mod tests {
     #[test]
     fn uncurrying() {
         let x = cps_expr!(fix f(x c)=(fix g(b k)=(+ [x b] [r] [(k r)]) in (c g)) in f);
-        let y = cps_expr!(fix f(x__1 c__2)=(fix g__3(b__4 k__5)=(f__0 x__1 c__2 g__3 b__4 k__5) in (c__2 g__3)); f__0(x c g b k)=(+ [x b] [r] [(k r)]) in f);
+        let y = cps_expr!(
+            fix f(x__1 c__2)=
+                    (fix g__3(b__4 k__5)=
+                            (record [x__1 c__2 g__3 b__4] args__6 (f__0 args__6 k__5))
+                      in (c__2 g__3));
+                f__0(args__7 k)=
+                    (select 0 args__7 x
+                        (select 1 args__7 c
+                            (select 2 args__7 g
+                                (select 3 args__7 b
+                                    (+ [x b] [r] [(k r)])))))
+            in f);
         assert_eq!(uncurry(x), y);
     }
 }
