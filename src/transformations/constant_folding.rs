ad4919e7c41cc1e642e424516e8dccc9985acc11
Martin Billinger <mbillingr@users.noreply.github.com>
2023-11-05 21:38:11 +0100

    more constant folding

--- src/transformations/constant_folding.rs
+++ src/transformations/constant_folding.rs
@@ -33,19 +33,33 @@ impl<V: Clone + PartialEq, F: Clone + PartialEq> Transform<V, F> for ConstantFol
                 Ref([no, _]),
             ) => Transformed::Again((**no).clone()),
 
+            Expr::PrimOp(PrimOp::ILess, Ref([Value::Int(a), Value::Int(b)]), _, Ref([no, yes])) => {
+                if a < b {
+                    Transformed::Again((**yes).clone())
+                } else {
+                    Transformed::Again((**no).clone())
+                }
+            }
+
+            Expr::PrimOp(PrimOp::ILess, Ref([Value::Var(a), Value::Var(b)]), _, Ref([no, _]))
+                if a == b =>
+            {
+                Transformed::Again((**no).clone())
+            }
+
             Expr::PrimOp(
                 PrimOp::IAdd,
                 Ref([Value::Int(a), Value::Int(b)]),
                 Ref([var]),
                 Ref([cnt]),
-            ) => Transformed::Done((**cnt).substitute_var(var, &Value::Int(*a + *b))),
+            ) => Transformed::Again((**cnt).substitute_var(var, &Value::Int(*a + *b))),
 
             Expr::PrimOp(PrimOp::IAdd, Ref([Value::Int(0), x]), Ref([var]), Ref([cnt])) => {
-                Transformed::Done((**cnt).substitute_var(var, x))
+                Transformed::Again((**cnt).substitute_var(var, x))
             }
 
             Expr::PrimOp(PrimOp::IAdd, Ref([x, Value::Int(0)]), Ref([var]), Ref([cnt])) => {
-                Transformed::Done((**cnt).substitute_var(var, x))
+                Transformed::Again((**cnt).substitute_var(var, x))
             }
 
             Expr::PrimOp(
@@ -53,10 +67,10 @@ impl<V: Clone + PartialEq, F: Clone + PartialEq> Transform<V, F> for ConstantFol
                 Ref([Value::Int(a), Value::Int(b)]),
                 Ref([var]),
                 Ref([cnt]),
-            ) => Transformed::Done((**cnt).substitute_var(var, &Value::Int(*a - *b))),
+            ) => Transformed::Again((**cnt).substitute_var(var, &Value::Int(*a - *b))),
 
             Expr::PrimOp(PrimOp::ISub, Ref([x, Value::Int(0)]), Ref([var]), Ref([cnt])) => {
-                Transformed::Done((**cnt).substitute_var(var, x))
+                Transformed::Again((**cnt).substitute_var(var, x))
             }
 
             Expr::PrimOp(
@@ -64,22 +78,22 @@ impl<V: Clone + PartialEq, F: Clone + PartialEq> Transform<V, F> for ConstantFol
                 Ref([Value::Int(a), Value::Int(b)]),
                 Ref([var]),
                 Ref([cnt]),
-            ) => Transformed::Done((**cnt).substitute_var(var, &Value::Int(*a * *b))),
+            ) => Transformed::Again((**cnt).substitute_var(var, &Value::Int(*a * *b))),
 
             Expr::PrimOp(PrimOp::IMul, Ref([x, Value::Int(1)]), Ref([var]), Ref([cnt])) => {
-                Transformed::Done((**cnt).substitute_var(var, x))
+                Transformed::Again((**cnt).substitute_var(var, x))
             }
 
             Expr::PrimOp(PrimOp::IMul, Ref([Value::Int(1), x]), Ref([var]), Ref([cnt])) => {
-                Transformed::Done((**cnt).substitute_var(var, x))
+                Transformed::Again((**cnt).substitute_var(var, x))
             }
 
             Expr::PrimOp(PrimOp::IMul, Ref([_, Value::Int(0)]), Ref([var]), Ref([cnt])) => {
-                Transformed::Done((**cnt).substitute_var(var, &Value::Int(0)))
+                Transformed::Again((**cnt).substitute_var(var, &Value::Int(0)))
             }
 
             Expr::PrimOp(PrimOp::IMul, Ref([Value::Int(0), _]), Ref([var]), Ref([cnt])) => {
-                Transformed::Done((**cnt).substitute_var(var, &Value::Int(0)))
+                Transformed::Again((**cnt).substitute_var(var, &Value::Int(0)))
             }
 
             Expr::PrimOp(
@@ -87,10 +101,10 @@ impl<V: Clone + PartialEq, F: Clone + PartialEq> Transform<V, F> for ConstantFol
                 Ref([Value::Int(a), Value::Int(b)]),
                 Ref([var]),
                 Ref([cnt]),
-            ) => Transformed::Done((**cnt).substitute_var(var, &Value::Int(*a / *b))),
+            ) => Transformed::Again((**cnt).substitute_var(var, &Value::Int(*a / *b))),
 
             Expr::PrimOp(PrimOp::IDiv, Ref([x, Value::Int(1)]), Ref([var]), Ref([cnt])) => {
-                Transformed::Done((**cnt).substitute_var(var, x))
+                Transformed::Again((**cnt).substitute_var(var, x))
             }
 
             _ => Transformed::Continue,
@@ -114,7 +128,7 @@ mod tests {
     }
 
     #[test]
-    fn fold_constant_comparison() {
+    fn fold_constant_equality() {
         let x = Expr::from_str("(primop = (0 0) () ((no) (yes)))").unwrap();
         let y = Expr::from_str("(yes)").unwrap();
         assert_eq!(ConstantFolder.transform_expr(&x), y);
@@ -138,6 +152,30 @@ mod tests {
         let x = Expr::from_str("(primop =s (\"foo\" \"bar\") () ((no) (yes)))").unwrap();
         let y = Expr::from_str("(no)").unwrap();
         assert_eq!(ConstantFolder.transform_expr(&x), y);
+    }
+
+    #[test]
+    fn fold_integer_comparison() {
+        let x = Expr::from_str("(primop < (0 0) () ((no) (yes)))").unwrap();
+        let y = Expr::from_str("(no)").unwrap();
+        assert_eq!(ConstantFolder.transform_expr(&x), y);
+
+        let x = Expr::from_str("(primop < (0 1) () ((no) (yes)))").unwrap();
+        let y = Expr::from_str("(yes)").unwrap();
+        assert_eq!(ConstantFolder.transform_expr(&x), y);
+
+        let x = Expr::from_str("(primop < (x x) () ((no) (yes)))").unwrap();
+        let y = Expr::from_str("(no)").unwrap();
+        assert_eq!(ConstantFolder.transform_expr(&x), y);
+
+        let x = Expr::from_str("(primop < (x y) () ((no) (yes)))").unwrap();
+        assert_eq!(ConstantFolder.transform_expr(&x), x);
+
+        let x = Expr::from_str("(primop < (0 x) () ((no) (yes)))").unwrap();
+        assert_eq!(ConstantFolder.transform_expr(&x), x);
+
+        let x = Expr::from_str("(primop < (x 0) () ((no) (yes)))").unwrap();
+        assert_eq!(ConstantFolder.transform_expr(&x), x);
     }
 
     #[test]
@@ -204,6 +242,11 @@ mod tests {
 
         let x = Expr::from_str("(primop * (0 x) (r) ((halt r)))").unwrap();
         let y = Expr::from_str("(halt 0)").unwrap();
+        assert_eq!(ConstantFolder.transform_expr(&x), y);
+
+        // fold-cascade
+        let x = Expr::from_str("(primop + (1 2) (y) ((primop + (y y) (z) ((halt z)))))").unwrap();
+        let y = Expr::from_str("(halt 6)").unwrap();
         assert_eq!(ConstantFolder.transform_expr(&x), y);
     }
 }
